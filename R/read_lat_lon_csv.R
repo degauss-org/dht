7ec4bd41a0ce110e81590b376ca5c5e50cf22517
Erika <erika.rasnick@gmail.com>
2021-02-15 14:57:43 -0500

    add nest_df argument to read_lat_lon_csv

--- R/read_lat_lon_csv.R
+++ R/read_lat_lon_csv.R
@@ -2,6 +2,7 @@
 #'
 #' @export
 #' @param filename name of input file, probably opt$filename if inside container
+#' @param nest_df logical. If TRUE, data is nested on lat/lon. Defaults to FALSE.
 #' @param sf_out logical. If TRUE, data is converted as an sf object. Defaults to FALSE.
 #' @param project_to_crs (optional) if sf_out=TRUE, the crs to which input data is projected. If unspecified
 #'                       and sf_out=TRUE, the crs defaults to 4326.
@@ -15,27 +16,30 @@
 #'                       sf_out = TRUE, project_to_crs = 5072)
 #' }
 
-read_lat_lon_csv <- function(filename, sf_out=FALSE, project_to_crs=NULL) {
+read_lat_lon_csv <- function(filename, nest_df=FALSE, sf_out=FALSE, project_to_crs=NULL) {
   cli::cli_alert_info('loading input file...')
   raw_data <- suppressMessages(readr::read_csv(filename))
-
   raw_data$.row <- seq_len(nrow(raw_data))
-  d <-
-    raw_data %>%
-    dplyr::select(.row, lat, lon) %>%
-    stats::na.omit() %>%
-    dplyr::group_by(lat, lon) %>%
-    tidyr::nest(.rows = c(.row))
 
-  if(sf_out) {
-    cli::cli_alert_info('converting input to sf object...')
-    d <- sf::st_as_sf(d, coords = c('lon', 'lat'), crs = 4326)
+  if(nest_df) {
+    d <-
+      raw_data %>%
+      dplyr::select(.row, lat, lon) %>%
+      stats::na.omit() %>%
+      dplyr::group_by(lat, lon) %>%
+      tidyr::nest(.rows = c(.row))
 
-    if(!is.null(project_to_crs)) {
-      cli::cli_alert_info('projecting input...')
-      d <- sf::st_transform(d, project_to_crs)
+    if(sf_out) {
+      cli::cli_alert_info('converting input to sf object...')
+      d <- sf::st_as_sf(d, coords = c('lon', 'lat'), crs = 4326)
+
+      if(!is.null(project_to_crs)) {
+        cli::cli_alert_info('projecting input...')
+        d <- sf::st_transform(d, project_to_crs)
+      }
     }
+    return(list(raw_data = raw_data, d = d))
   }
 
-  return(list(raw_data = raw_data, d = d))
+  return(raw_data)
 }
