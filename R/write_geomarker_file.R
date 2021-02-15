92634af60481f351adc23df7113fd0fd31dee24a
Erika <erika.rasnick@gmail.com>
2021-02-15 15:04:37 -0500

    update write_geomarker_file to accomodate non-nested inputs

--- R/write_geomarker_file.R
+++ R/write_geomarker_file.R
@@ -2,7 +2,8 @@
 #'
 #' @export
 #' @param d input nest on .row with added geomarker column(s)
-#' @param raw_data original unnested input data
+#' @param raw_data original unnested input data, defaults to NULL (for use when
+#'                 nest_df = FALSE in read_lat_lon_csv)
 #' @param filename name of input file, probably opt$filename if inside container
 #' @param geomarker_name name of the geomarker, must be the name used in the degauss.org url
 #' @param version container version number as a character string
@@ -14,11 +15,13 @@
 #'            geomarker='roads', version='0.4')
 #' }
 
-write_geomarker_file <- function(d, raw_data, filename, geomarker_name, version) {
-  d <- tidyr::unnest(d, cols = c(.rows))
-  if('sf' %in% class(d)) d <- sf::st_drop_geometry(d)
+write_geomarker_file <- function(d, raw_data = NULL, filename, geomarker_name, version) {
+  if (!is.null(raw_data)) {
+    d <- tidyr::unnest(d, cols = c(.rows))
+    if('sf' %in% class(d)) d <- sf::st_drop_geometry(d)
+    out <- dplyr::left_join(raw_data, d, by = '.row') %>% dplyr::select(-.row)
+  }
 
-  out <- dplyr::left_join(raw_data, d, by = '.row') %>% dplyr::select(-.row)
   out_file_name <- glue::glue('{tools::file_path_sans_ext(filename)}_{geomarker_name}_v{version}.csv')
   readr::write_csv(out, out_file_name)
   cli::cli_alert_success('\nFINISHED! output written to {out_file_name}')
