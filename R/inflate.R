03d1822f3dc401bef279eca9276cca0f986a6ddb
Cole Brokamp <cole.brokamp@gmail.com>
2021-10-15 16:20:13 -0400

    group inflation/template functions in same file

--- R/inflate.R
+++ R/inflate.R
@@ -1,7 +1,55 @@
+#' Add degauss.Rmd and Makefile for DeGAUSS container development
+#'
+#' @param geomarker Path where to save file
+#'
+#' @return
+#' Create a degauss_template.Rmd file and return its path
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' add_degauss_template(geomarker = geomarker_example)
+#' unlink(geomarker_example, recursive = TRUE)
+#' }
+add_degauss_template <- function(geomarker = ".") {
+
+  project_name <- basename(normalizePath(geomarker))
+  geomarker <- normalizePath(geomarker)
+
+  # rmd file
+  rmd_path <- file.path(geomarker, "degauss.Rmd")
+
+  if (file.exists(rmd_path)) {
+    cli::cli_abort("A degauss.Rmd file already exists at {rmd_path}")
+  }
+
+  rmd_template <-
+    system.file("degauss.Rmd", package = "dht") %>%
+    readLines()
+
+  # make any geomarker specific changes here
+
+  cat(enc2utf8(rmd_template), file = rmd_path, sep = "\n")
+
+  cli::cli_alert_success("degauss.Rmd added at {rmd_path}")
+
+  # makefile
+  makefile_path <- file.path(geomarker, "Makefile")
+
+  if (file.exists(makefile_path)) {
+    cli::cli_abort("A Makefile already exists at {makefile_path}")
+  }
+
+  file.copy(from = system.file("Makefile", package = "dht"), to = makefile_path)
+
+  cli::cli_alert_success("Makefile added at {makefile_path}")
+
+  cli::cli_alert_info("After editing degauss.Rmd, run `make build` to inflate and build a DeGAUSS container")
+}
+
 #' Inflate Rmd to degauss files
 #'
-#' @param geomarker Path to container
-#' @param rmd Path to Rmarkdown file to inflate
+#' @param geomarker path to folder containing degauss.Rmd
 #' @param test Logical. Whether to test container after Rmd inflating
 #' @param overwrite Logical. Whether to overwrite vignette and functions if already exists.
 #'
@@ -82,10 +130,5 @@ inflate <- function(geomarker = ".", rmd = file.path("dev", "degauss_template.Rm
       file = 'readme.md'
   )
 }
-
-
-
-
-
 
 
