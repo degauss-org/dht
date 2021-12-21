446b75891a7c43a05dec32e3611698103a5d0496
Cole Brokamp <cole-brokamp@users.noreply.github.com>
2021-12-21 18:09:58 -0500

    add function to get degauss env metadata online (#45)

--- R/degauss_env.R
+++ R/degauss_env.R
@@ -1,22 +1,26 @@
 #' get environment variables from a Dockerfile
 #'
-#' This function looks in a Dockerfile to extract specified environment variables and their values.
+#' This functions look in a Dockerfile to extract specified environment variables and their values.
+#' `get_env_from_dockerfile` expects a path to a Dockerfile, but `get_env_from_degauss_online`
+#' takes the name of DeGAUSS container that is used to download the corresponding Dockerfile.
 #' It assumes each `ENV` instruction is on its own line and defines only one environment variable.
 #' The default names of environment variables assumes those used for DeGAUSS metadata.
 #'
-#' @export
 #' @param dockerfile_path path to Dockerfile
 #' @param env_names names of environment variables to extract
+#' @param name name of DeGAUSS container to download Dockerfile from
 #' @examples
 #' \dontrun{
 #' use_degauss_dockerfile(version = "0.1")
 #' get_env_from_dockerfile()
 #' get_env_from_dockerfile()["degauss_version"]
+#' get_env_from_degauss_online("fortunes")
+#' get_env_from_degauss_online("fortunes")["degauss_version"]
 #' }
 
+#' @export
 get_env_from_dockerfile <- function(dockerfile_path = fs::path_join(c(getwd(), "Dockerfile")),
                                     env_names = c("degauss_name", "degauss_version", "degauss_description")) {
-
   env_values <-
     dockerfile_path %>%
     normalizePath(mustWork = TRUE) %>%
@@ -27,4 +31,17 @@ get_env_from_dockerfile <- function(dockerfile_path = fs::path_join(c(getwd(), "
 
   names(env_values) <- env_names
   return(env_values)
+}
+
+#' @export
+#' @rdname get_env_from_dockerfile
+get_env_from_degauss_online <- function(name = "fortunes") {
+  withr::with_tempfile("df", {
+    utils::download.file(
+      glue::glue("https://raw.githubusercontent.com/degauss-org/{name}/main/Dockerfile"),
+      df,
+      quiet = TRUE
+    )
+    get_env_from_dockerfile(df)
+  })
 }
--- R/globals.R
+++ R/globals.R
@@ -1,1 +1,1 @@
-utils::globalVariables(c("lat", "lon", ".rows", "start_date", "end_date"))
+utils::globalVariables(c("lat", "lon", ".rows", "start_date", "end_date", "df"))
