dcf0aca2954c14a2f109b85740cccc26c3e7f79f
Cole Brokamp <cole.brokamp@gmail.com>
2022-01-28 16:05:52 -0500

    change get_degauss_env_* functions
    - new user interface: instead of specifying the names of the desired
    environment variables, the function returns only those that are
    prefixed with "degauss_"

--- R/degauss_env.R
+++ R/degauss_env.R
@@ -1,53 +1,55 @@
 #' get environment variables from a Dockerfile
 #'
-#' These functions look in a Dockerfile to extract specified environment variables and
-#' their values corresponding to DeGAUSS container metadata.
-#' `get_env_from_dockerfile` expects a path to a Dockerfile, but `get_env_from_degauss_online`
+#' These functions look in a Dockerfile to extract environment variables corresponding to DeGAUSS container metadata.
+#' They look for ENV instructions where the name begins with "degauss_";
+#' for example "degauss_name", or "degauss_version".
+#' `get_degauss_env_dockerfile` expects a path to a Dockerfile, but `get_degauss_env_online`
 #' takes the name of DeGAUSS container that is used to download the corresponding Dockerfile.
-#' It assumes each `ENV` instruction is on its own line,
-#' defines only one environment variable, and is always in the order:
-#' "degauss_name", "degauss_version", "degauss_description", "degauss_argument"
+#' It assumes each `ENV` instruction is on its own line and defines only one environment variable.
 #'
 #' @param dockerfile_path path to Dockerfile
 #' @param name name of DeGAUSS container to download Dockerfile from
-#' @return named list of DeGAUSS metatdata elements:
-#' "degauss_name", "degauss_version", "degauss_description", "degauss_argument"
+#' @return named vector of DeGAUSS metatdata elements
 #' @examples
 #' \dontrun{
 #' use_degauss_dockerfile(version = "0.1")
-#' get_env_from_dockerfile()
-#' get_env_from_dockerfile()["degauss_version"]
-#' get_env_from_degauss_online("fortunes")
-#' get_env_from_degauss_online("fortunes")["degauss_version"]
+#' get_degauss_env_dockerfile()
+#' get_degauss_env_dockerfile()["degauss_version"]
+#' get_degauss_env_online("fortunes")
+#' get_degauss_env_online("fortunes")["degauss_version"]
 #' }
 
 #' @export
-get_env_from_dockerfile <- function(dockerfile_path = fs::path_join(c(getwd(), "Dockerfile"))) {
+get_degauss_env_dockerfile <- function(dockerfile_path = fs::path_join(c(getwd(), "Dockerfile"))) {
 
-  env_names <- c("degauss_name", "degauss_version", "degauss_description", "degauss_argument")
-
-  env_values <-
+  env_text <-
     dockerfile_path %>%
     normalizePath(mustWork = TRUE) %>%
     readLines(warn = FALSE) %>%
-    stringr::str_subset(pattern = stringr::fixed("ENV ")) %>%
-    stringr::str_subset(pattern = stringr::fixed("#"), negate = TRUE) %>%
-    stringr::str_extract(glue::glue("(?<={env_names}=).*")) %>%
-    stringr::str_remove_all("[^[:alnum:] _.]")
+    stringr::str_subset(stringr::fixed("#"), negate = TRUE) %>%
+    stringr::str_subset(stringr::fixed("ENV ")) %>%
+    stringr::str_subset(stringr::fixed("degauss_")) %>%
+    stringr::str_extract("[degauss_].*") %>%
+    stringr::str_split(stringr::fixed("="), n = 2, simplify = FALSE)
 
-  names(env_values) <- env_names
+  env_values <-
+    purrr::map_chr(env_text, 2) %>%
+    stringr::str_remove_all(stringr::fixed('\"')) %>%
+    purrr::set_names(purrr::map_chr(env_text, 1))
+
   return(env_values)
 }
 
+
 #' @export
-#' @rdname get_env_from_dockerfile
-get_env_from_degauss_online <- function(name = "fortunes") {
+#' @rdname get_degauss_env_dockerfile
+get_degauss_env_online <- function(name = "fortunes") {
   withr::with_tempfile("df", {
     utils::download.file(
       glue::glue("https://raw.githubusercontent.com/degauss-org/{name}/main/Dockerfile"),
       df,
       quiet = TRUE
     )
-    get_env_from_dockerfile(df)
+    get_degauss_env_dockerfile(df)
   })
 }
