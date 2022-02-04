0c104be55d8313287230638168d4b5ecdb1e4d0e
Cole Brokamp <cole.brokamp@gmail.com>
2022-02-03 22:34:50 -0500

    adds optional argument info to output file name; closes #60

--- R/write_geomarker_file.R
+++ R/write_geomarker_file.R
@@ -5,10 +5,11 @@
 #' @param raw_data original unnested input data, defaults to NULL (for use when
 #'                 nest_df = FALSE in read_lat_lon_csv)
 #' @param filename name of input file, probably opt$filename if inside container
-#' @param geomarker_name name of the geomarker, must be the name used in the degauss.org url;
-#' defaults to degauss environment variable `degauss_name`
+#' @param geomarker_name name of the geomarker; defaults to degauss environment variable `degauss_name`
 #' @param version container version number as a character string; defaults to degauss environment
 #' variable `degauss_version`
+#' @param argument optional information to append after the image version number that was specified using a degauss argument;
+#' for example, a selected buffer radius, care site, or geocoding threshold
 #' @return output file is written to working directory
 #' @examples
 #' \dontrun{
@@ -21,7 +22,8 @@ write_geomarker_file <- function(d,
                                  raw_data = NULL,
                                  filename,
                                  geomarker_name = Sys.getenv("degauss_name"),
-                                 version = Sys.getenv("degauss_version")) {
+                                 version = Sys.getenv("degauss_version"),
+                                 argument = NULL) {
   if (!is.null(raw_data)) {
     d <- tidyr::unnest(d, cols = c(.rows))
     if('sf' %in% class(d)) d <- sf::st_drop_geometry(d)
@@ -32,7 +34,11 @@ write_geomarker_file <- function(d,
     out <- d
   }
 
-  out_file_name <- glue::glue('{tools::file_path_sans_ext(filename)}_{geomarker_name}_{version}.csv', wrap = TRUE)
+  out_file_name <- glue::glue('{tools::file_path_sans_ext(filename)}_{geomarker_name}_{version}', wrap = TRUE)
+  if (!is.null(argument)) {
+    out_file_name <- glue::glue("{out_file_name}_{argument}")
+  }
+  out_file_name <- glue::glue("{out_file_name}.csv")
   readr::write_csv(out, out_file_name)
   cli::cli_alert_success('\nFINISHED! output written to {out_file_name}', wrap = TRUE)
 }
