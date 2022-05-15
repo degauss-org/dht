1dca9f7b56145ce6302d37a4145bc2f62c66c59b
Cole Brokamp <cole.brokamp@gmail.com>
2022-05-14 20:52:00 -0400

    only do geomarkers in the shiny app, no geocoding

--- inst/degauss-menu/app.R
+++ inst/degauss-menu/app.R
@@ -13,27 +13,23 @@ ui <- dashboardPage(
     color = dht::degauss_colors(5)
   ),
   body = dashboardBody(
+
     box(
-      checkboxInput("geocode", "Geocode addresses?", FALSE),
-      textInput("address_filename", "Address filename", value = "my_address_file.csv"),
-      htmltools::h6("DeGAUSS geocoding command:"),
-      verbatimTextOutput("degauss_geocoding_cmd", placeholder = TRUE),
-      title = "Geocoding",
-      width = 12,
-      collapsed = TRUE
-    ),
-    box(
+      title = "Input File",
       textInput("input_filename", "Geocoded filename", value = "my_address_file_geocoded.csv"),
-      checkboxGroupInput(
-        "selected_images",
-        label = "Select which DeGAUSS images to use:",
-        choices = dht:::core_lib_images(geocoder = FALSE)
-      ),
-      htmltools::h6("DeGAUSS command(s):"),
+      bookmarkButton("Copy URL to save app state"),
+      width = 6
+    ),
+    box(DT::dataTableOutput("core_lib_images_table"),
+        title = "DeGAUSS Core Library",
+        width = 12
+        ),
+    box(
       verbatimTextOutput("degauss_cmd", placeholder = TRUE),
-      title = "Geomarker Assessment",
+      title = "DeGAUSS command(s)",
       width = 12
     )
+
   )
 )
 
@@ -43,24 +39,35 @@ ui <- dashboardPage(
 # TODO can we leave the default of "none" for drivetime, or let it error with an empty value?
 # TODO add function in package to run this app, suggest and check for shiny installation first
 
+
 server <- function(input, output, session) {
-  d <- dht::create_degauss_menu_data()
+
+  d <-
+    get_degauss_core_lib_env(geocoder = FALSE) %>%
+    dht::create_degauss_menu_data()
+
+  output$core_lib_images_table <-
+    DT::renderDataTable(
+      dplyr::select(d, -degauss_cmd),
+      escape = FALSE,
+      options = list(autoWidth = TRUE)
+    )
+
   output$degauss_cmd <- renderText({
+    .r <- input$core_lib_images_table_rows_selected
+    if (length(.r)) {
       d %>%
-        dplyr::filter(name %in% input$selected_images) %>%
+        ## dplyr::filter(name %in% input$selected_images) %>%
+        dplyr::slice(input$core_lib_images_table_rows_selected) %>%
         dplyr::pull(degauss_cmd) %>%
-        gsub("my_address_file_geocoded.csv", input$input_filename, ., fixed = TRUE)
+        gsub("my_address_file_geocoded.csv",
+             input$input_filename,
+             .,
+             fixed = TRUE)
+    }
     },
     sep = "\n"
   )
-  output$degauss_geocoding_cmd <- renderText({
-    if (input$geocode) {
-      d %>%
-        dplyr::filter(name == "geocoder") %>%
-        dplyr::pull(degauss_cmd) %>%
-        gsub("my_address_file_geocoded.csv", input$address_filename, ., fixed = TRUE)
-    }
-  })
 }
 
 shinyApp(ui, server)
